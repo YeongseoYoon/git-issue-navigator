5b16619aff6407b4481f4374527d165535be282f
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-08-30 03:44:52 +0900

    💄 design: 헤더 디자인 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,10 @@ import { ORGANIZATION_NAME, REPOSITORY_NAME } from '../constants/constants';
 
 const Header = () => {
   return (
-    <header className="p-2 border border-b-2">
-      <div className="flex max-w-2xl mx-auto justify-center">
+    <header className="flex items-center justify-center p-2 border-gray-400 border border-t-0 h-12">
+      <div className="flex mx-auto justify-center">
         <h1 className="text-lg font-semibold">{ORGANIZATION_NAME}</h1>
         <h1 className="text-lg font-semibold mx-1">/</h1>
-
         <h1 className="text-lg font-semibold">{REPOSITORY_NAME}</h1>
       </div>
     </header>
