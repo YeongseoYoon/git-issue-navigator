3781955fc62cb74610475a53237d9211ba1190de
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-08-30 03:14:45 +0900

    💄 tailwindcss 전역 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <Header />
+      <Outlet />
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 
-import App from "./App";
+import router from './router/Router';
+import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
