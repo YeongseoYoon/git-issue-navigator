1d48c2dd06f2f04d2bcdab93e864f64a034bc32e
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-08-30 17:11:28 +0900

    ✨ feat: 다섯번째 셀마다 광고 이미지 호출

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,8 @@
 export const ORGANIZATION_NAME = 'Facebook';
 
 export const REPOSITORY_NAME = 'React';
+
+export const IMAGE_URL =
+  'https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100';
+
+export const IMAGE_HREF = 'https://www.wanted.co.kr/';
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { useOctokitFetch } from '../../hooks/useOctokitFetch';
-import { ORGANIZATION_NAME, REPOSITORY_NAME } from '../../constants/constants';
+import {
+  IMAGE_HREF,
+  IMAGE_URL,
+  ORGANIZATION_NAME,
+  REPOSITORY_NAME,
+} from '../../constants/constants';
 import { Issue } from '../../types/issue';
 import IssueItem from '../../components/IssueItem';
 
@@ -18,16 +23,25 @@ const Home = () => {
 
   return (
     <>
-      {issues?.map(issue => (
-        <Link
-          key={`${issue?.id}-${issue?.created_at}`}
-          to={`/detail/${issue?.number}`}
-          className="cursor-pointer"
-        >
-          <div className="flex flex-row m-3 items-center">
-            <IssueItem issue={issue} />
-          </div>
-        </Link>
+      {issues?.map((issue, index) => (
+        <>
+          <Link
+            key={`${issue?.id}-${issue?.created_at}`}
+            to={`/detail/${issue?.number}`}
+            className="cursor-pointer"
+          >
+            <div className="flex flex-row m-3 items-center">
+              <IssueItem issue={issue} />
+            </div>
+          </Link>
+          {index % 5 === 4 && (
+            <a href={IMAGE_HREF} target="_blank" rel="noopener noreferrer">
+              <div className="w-full flex m-3 cursor-pointer justify-center border">
+                <img src={IMAGE_URL} alt="Ad-Image" />
+              </div>
+            </a>
+          )}
+        </>
       ))}
     </>
   );
